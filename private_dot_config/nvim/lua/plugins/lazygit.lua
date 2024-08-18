dcd709f021c7d1b7f059dfebf415c5d70727d96a
Arjun K <arjunk03@users.noreply.github.com>
2024-08-18 21:06:18 +0530

    Update .config/nvim/lazy-lock.json Update .config/nvim/lazyvim.json Add .config/nvim/lua/plugins/lazygit.lua

---
+++ private_dot_config/nvim/lua/plugins/lazygit.lua
@@ -0,0 +1,23 @@
+-- since this is just an example spec, don't actually load anything here and return an empty spec
+-- stylua: ignore
+
+
+return {
+  "kdheepak/lazygit.nvim",
+  cmd = {
+    "LazyGit",
+    "LazyGitConfig",
+    "LazyGitCurrentFile",
+    "LazyGitFilter",
+    "LazyGitFilterCurrentFile",
+  },
+  -- optional for floating window border decoration
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  -- setting the keybinding for LazyGit with 'keys' is recommended in
+  -- order to load the plugin when the command is run for the first time
+  keys = {
+    { "<leader>gg ", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+  }
+}
